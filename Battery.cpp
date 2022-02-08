49f7a293c6411df768696519ef03baa0e297a6b8
waseem-syed <91089691+waseem-syed@users.noreply.github.com>
2022-02-08 22:59:39 +0530

    Add files via upload

---
+++ Battery.cpp
@@ -0,0 +1,56 @@
+#include "BatteryCheck.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void Battery::printWarning(string warningType)
+{
+    cout << warningType.append(" is out of range!") << ::endl;
+}
+
+bool Battery::isBatteryOK(float temperature, float soc, float chargeRate)
+{
+   bool isBatteryOk = false;
+   if(checkTemperatureRange(temperature) && checkStateOfCharegeRange(soc) && checkChargeRateRange(chargeRate))
+   {
+      isBatteryOk = true;
+   }
+    return isBatteryOk;
+}
+
+bool Battery::checkTemperatureRange(float temperatue)
+{
+   bool isTemperatureInRange = true;
+   if(false == (temperatue > MIN_TEMPERATURE && temperatue <= MAX_TEMPERATURE))
+   {
+      isTemperatureInRange = false;
+	  string temperatue("TEMPERATURE");
+	  printWarning(temperatue);
+   }
+   return isTemperatureInRange;
+}
+
+bool Battery::checkStateOfCharegeRange(float soc)
+{
+   bool isSOCInRange = true;  
+   if(false == (soc >= MIN_SOC && soc <= MAX_SOC))
+   {
+      isSOCInRange = false;
+      string soc("SOC");
+      printWarning(soc);
+   }
+   return isSOCInRange;
+}
+
+bool Battery::checkChargeRateRange(float rate)
+{
+   bool isChargeRateInRange = true;
+   if(false == (rate <= CHARGE_RATE))
+   {
+      isChargeRateInRange false;
+      string chargeRate("CHARGE RATE");
+      printWarning(chargeRate); 
+   }
+   return isChargeRateInRange;
+}
