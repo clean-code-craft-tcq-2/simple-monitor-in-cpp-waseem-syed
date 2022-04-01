4c1473c6a7c3e9df8769bad1222b60e90ac0c13a
waseem-syed <91089691+waseem-syed@users.noreply.github.com>
2022-04-01 21:04:30 +0530

    Update BatteryManagement.cpp

--- BatteryManagement.cpp
+++ BatteryManagement.cpp
@@ -9,8 +9,8 @@ int main() {
    test.testBattery(       40     ,   80  ,     0.7      ,     true     );
    test.testBattery(       40     ,   50  ,     0.8      ,     false    );
    test.testBattery(       40     ,   50  ,     0.7      ,     true     );
-   test.testBattery(       45     ,   20  ,     0.7      ,     true   );
-   test.testBattery(       45     ,   80  ,     0.8      ,     true     );
+   test.testBattery(       45     ,   20  ,     0.7      ,     true     );
+   test.testBattery(       45     ,   80  ,     0.2      ,     true     );
    test.testBattery(       0      ,   20  ,     0.7      ,     false    );
    test.testBattery(       40     ,   60  ,     0.9      ,     false    );
    test.testBattery(       40     ,   90  ,     0.7      ,     false    );
